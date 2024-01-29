4474a0a1c33323ca59cc128ca38be6a463b65457
Rodrigo Souza <56081755+RodrigoSouza-Dev@users.noreply.github.com>
2024-01-29 12:54:40 -0300

    Update redu_dim.py

--- redu_dim.py
+++ redu_dim.py
@@ -3,6 +3,7 @@ import numpy as np
 #A linha de codigo abaixo é utilizada pra recurso do google Colab
 from google.colab.patches import cv2_imshow
 
+
 # Carrega a imagem colorida
 imagem_colorida = cv2.imread("working.jpg")
 
@@ -11,15 +12,13 @@ if imagem_colorida is None:
     print("Erro ao carregar a imagem!")
     exit()
 
-print(imagem_colorida.shape)  # Print the shape to confirm dimensions
-
 # Converte a imagem colorida para tons de cinza
 imagem_gray = cv2.cvtColor(imagem_colorida, cv2.COLOR_BGR2GRAY)
 
-# Aplica o algoritmo de binarização
+# Aplica o algoritmo de binarização para criar uma imagem preto e branco
 limiar = 127
-_, imagem_binaria = cv2.threshold(imagem_gray, limiar, 255, cv2.THRESH_BINARY)
+_, imagem_bw = cv2.threshold(imagem_gray, limiar, 255, cv2.THRESH_BINARY)
 
 # Exibe as imagens
-cv2_imshow(imagem_colorida)
-cv2_imshow(imagem_binaria)
+cv2_imshow(imagem_gray)
+cv2_imshow(imagem_bw)
